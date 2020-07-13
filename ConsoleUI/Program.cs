789346cc9d2999f70500e7cab4296d7e0143a89d
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-13 11:28:49 -0400

    Initial commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static TableServers host1List = TableServers.GetTableServers();
+        static TableServers host2List = TableServers.GetTableServers();
+        static void Main(string[] args)
+        {
+            TableServers servers = TableServers.GetTableServers();
+            for (int i = 0; i < 5; i++)
+            {
+                //Console.WriteLine("The next server is: " + servers.GetNextServer());
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine("The next server is: " + host1List.GetNextServer());
+        }
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine("The next server is: " + host2List.GetNextServer());
+        }
+    }
+}
+
+
+
+
+
+// Code before applying Singleton Design Pattern
+
+//using System;
+
+//namespace ConsoleUI
+//{
+//    class Program
+//    {
+//        static TableServers host1List = new TableServers();
+//        static TableServers host2List = new TableServers();
+//        static void Main(string[] args)
+//        {
+//            TableServers servers = new TableServers();
+//            for (int i = 0; i < 5; i++)
+//            {
+//                //Console.WriteLine("The next server is: " + servers.GetNextServer());
+//                Host1GetNextServer();
+//                Host2GetNextServer();
+//            }
+
+//            Console.ReadLine();
+//        }
+
+//        private static void Host1GetNextServer()
+//        {
+//            Console.WriteLine("The next server is: " + host1List.GetNextServer());
+//        }
+//        private static void Host2GetNextServer()
+//        {
+//            Console.WriteLine("The next server is: " + host2List.GetNextServer());
+//        }
+//    }
+//}
---
+++ ConsoleUI/TableServers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class TableServers
+    {
+        private static readonly TableServers _instance = new TableServers();
+        private List<string> servers = new List<string>();
+        private int nextServer = 0;
+
+        private TableServers()
+        {
+            servers.Add("Iyad");
+            servers.Add("Tim");
+            servers.Add("Sue");
+            servers.Add("Mary");
+        }
+
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+
+        public string GetNextServer()
+        {
+            string output = servers[nextServer];
+
+            nextServer += 1;
+            if (nextServer >= servers.Count)
+            {
+                nextServer = 0;
+            }
+
+            return output;
+        }
+    }
+}
+
+
+// Code before applying Singleton Design Pattern
+
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace ConsoleUI
+//{
+//    public class TableServers
+//    {
+//        private List<string> servers = new List<string>();
+//        private int nextServer = 0;
+
+//        public TableServers()
+//        {
+//            servers.Add("Iyad");
+//            servers.Add("Tim");
+//            servers.Add("Sue");
+//            servers.Add("Mary");
+//        }
+
+//        public string GetNextServer()
+//        {
+//            string output = servers[nextServer];
+
+//            nextServer += 1;
+//            if (nextServer >= servers.Count)
+//            {
+//                nextServer = 0;
+//            }
+
+//            return output;
+//        }
+//    }
+//}
